7ad91a293b4f75d57a344b3e8977c3ac559dded6
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-28 10:31:38 -0700

    :sparkles: add machine models

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace SillyFactory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    [Required]
+    public string Description { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+  }
+}
